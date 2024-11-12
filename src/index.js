ad7dbe7227fe06030f65229ced8980c5a90f0ed2
Amine Elgaini <149828485+amineElgaini@users.noreply.github.com>
2024-11-12 22:02:41 +0100

    setup environment, express, mongoose

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
+
+// connect to db
+mongoose
+    .connect(process.env.URI)
+    .then(() => {
+        console.log("connected to database");
+        // listen to port
+        app.listen(process.env.PORT, () => {
+            console.log("listening for requests on port", process.env.PORT);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
