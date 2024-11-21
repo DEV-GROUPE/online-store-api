415c8443bb9257185f6e3b0bd59498eae70ce29f
abdelkabir <abdkabirneami@gmail.com>
2024-11-21 19:16:58 +0100

    add pagination plugin to category model and use it in category controller

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,7 +1,8 @@
 import  mongoose from "mongoose";
-
+import mongoosePaginate from 'mongoose-paginate-v2';
 const CategorySchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true },
 });
-
+// Add the plugin
+CategorySchema.plugin(mongoosePaginate);
 export default mongoose.model("Category", CategorySchema);
